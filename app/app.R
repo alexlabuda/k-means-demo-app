8f7a5082a1d24c2972ec6fa45371dc0ae32fe77f
Alex Labuda <alexlabuda@gmail.com>
2024-02-26 22:29:16 -0500

    code comments

--- app/app.R
+++ app/app.R
@@ -1,3 +1,6 @@
+
+# LIBRARIES ---------------------------------------------------------------
+
 library(shiny)
 library(tidyverse)
 library(bslib)
@@ -6,30 +9,42 @@ library(tidyquant)
 library(tidyclust)
 library(tidymodels)
 library(ClusterR)
+
+# set default theme for plots
 theme_set(theme_minimal())
 
+# read data used for the app
 pengs_tbl <- penguins
 iris_tbl  <- iris |> as_tibble() |> rename(species = Species)
 
-
+# create a list of datasets
 datasets <- list(
   "Penguins" = pengs_tbl,
   "Iris"     = iris_tbl
 )
 
+# select column names for scatter plot x, y-axis selections
 names_penguins <- datasets[["Penguins"]] |> select(where(is.numeric)) |> names()
 names_iris     <- datasets[["Iris"]] |> select(where(is.numeric)) |> names()
 
 
+# UI ----------------------------------------------------------------------
+
 ui <- page_sidebar(
+  
   title   = "K-Means Clustering Demo",
   theme   = bs_theme(bootswatch = "minty"),
+  
   sidebar = sidebar(
     width   = 450,
+    
+    # Heading and introduction text
     p("This interactive tool is designed to help you explore the application of the K-means clustering algorithm on different datasets. By visualizing how data points are grouped into clusters, you can gain insights into data patterns and relationships.",
       style = "font-size: 15px"),
     br(),
     h4("Settings", style = "border-bottom: 1px solid #cccccc; padding-bottom: 5px; font-size: 16px"),
+    
+    # Inputs for dataset and number of clusters
     selectInput(
       inputId  = "dataset",
       label    = "Choose a dataset",
@@ -44,6 +59,8 @@ ui <- page_sidebar(
       min     = 1,
       max     = 5 
     ),
+    
+    # Conditional panel selections based on dataset selected
     br(),
     h4("Select columns for plots", style = "border-bottom: 1px solid #cccccc; padding-bottom: 5px; font-size: 16px"),
     conditionalPanel(
@@ -77,6 +94,8 @@ ui <- page_sidebar(
       )
     )
   ),
+  
+  # Main UI section
   layout_column_wrap(
     card(
       card_header("Original Data"),
@@ -102,12 +121,12 @@ server <- function(input, output) {
       drop_na()
   })
   
-  
+  # set plotting defaults
   legend_loc <- "top"
   alpha_val  <- 0.5
   
   
-  
+  # Scatterplot 1 ---------------------------------------------------------
   # Dynamically adjust plot based on the dataset selected
   output$plot <- renderPlot({
     data <- selected_data()
@@ -132,36 +151,45 @@ server <- function(input, output) {
       tidyquant::scale_color_tq()
   })
   
-  
+  # K-means clustering ----------------------------------------------------
   output$cluster_plot <- renderPlot({
+    
     data <- selected_data() |> select(where(is.numeric))
+    
+    # Recipe preprocessing specification
     rec_spec <- recipe( ~ ., data = data) %>%
       step_normalize(all_numeric_predictors())
     
+    # Prep recipe to retrieve means and sds for reverse normalization
     prepped_rec <- prep(rec_spec)
-    norm_means <- prepped_rec$steps[[1]]$means
-    norm_sds   <- prepped_rec$steps[[1]]$sds
+    norm_means  <- prepped_rec$steps[[1]]$means
+    norm_sds    <- prepped_rec$steps[[1]]$sds
     
+    # K-means model specification
     kmeans_spec <- k_means(num_clusters = input$k) %>%
       set_engine("ClusterR")
     
+    # Create a workflow for recipe and model
     kmeans_wf <- workflow(prepped_rec, kmeans_spec)
     
-    kmeans_fit <- kmeans_wf %>%
-      fit(data = data)
+    # Fit the model
+    kmeans_fit <- kmeans_wf %>% fit(data = data)
     
+    # Extract cluster centroids for plotting
     centroids <- extract_centroids(kmeans_fit)
     
-    # Reverse normalization for centroids
+    # Select column names for reverse normalization
     numeric_columns <- names(select(data, where(is.numeric)))
+    
+    # Reverse normalization for centroids
     for (col in numeric_columns) {
-      centroids[[col]] <-
-        (centroids[[col]] * norm_sds[col]) + norm_means[col]
+      centroids[[col]] <- (centroids[[col]] * norm_sds[col]) + norm_means[col]
     }
     
+    # Augment the original data with cluster predictions
     km_result <- augment(kmeans_fit, data)
     
-    # Now, select user-defined columns for plotting
+    # Select user-defined columns for plotting
     x_col <-
       if (input$dataset == "Iris")
         input$iris_x
@@ -173,51 +201,64 @@ server <- function(input, output) {
     else
       input$penguins_y
     
+    # Rename the cluster column for plotting
     plot_data <- km_result %>%
       mutate(Cluster = factor(.pred_cluster))
     
+    # Plot the clustered data
     ggplot(plot_data, aes_string(x = x_col, y = y_col, color = "Cluster")) +
       geom_point(size = 3, alpha = alpha_val - 0.25) +
       geom_point(
         data = centroids,
         aes_string(x = x_col, y = y_col),
         color = "black",
-        size = 5,
+        size  = 5,
         shape = 15
       ) +
       labs(title = paste("Clustering on", input$dataset, "Dataset")) +
-      theme(legend.position = legend_loc,
-            legend.text = element_text(size = 12),
-            panel.grid = element_blank(),
-            axis.text = element_text(size = 12),
-            axis.title = element_text(size = 12)) +
+      theme(
+        legend.position = legend_loc,
+        legend.text     = element_text(size = 12),
+        panel.grid      = element_blank(),
+        axis.text       = element_text(size = 12),
+        axis.title      = element_text(size = 12)) +
       tidyquant::scale_color_tq()
   })
   
+  
+  # K-means tuning --------------------------------------------------------
+  # For selecting the best k clusters
   output$tune_plot <- renderPlot({
+    
     # Data
     data <- selected_data() |> select(where(is.numeric))
     
+    # Model specification
     kmeans_spec_tune <- k_means() %>%
       set_engine("ClusterR")
     
-    ## TUNING
+    # Tuning
     kmeans_spec_tuned <- kmeans_spec_tune %>%
       set_args(num_clusters = tune())
     
+    # Workflow
     kmeans_wf <- workflow() %>%
       add_model(kmeans_spec_tuned) %>%
       add_formula( ~ .)
     
+    # Bootstrap resampling
     set.seed(1234)
     x_boots <- bootstraps(data, times = 5)
     
+    # Create a grid of k clusters
     num_clusters_grid <- tibble(num_clusters = seq(1, 10))
     
+    # Tune the model
     tune_res <- tune_cluster(object    = kmeans_wf,
                              resamples = x_boots,
                              grid      = num_clusters_grid)
     
+    # Plot the tuning results
     tune_res %>%
       collect_metrics() |>
       filter(.metric == "sse_within_total") |>
@@ -233,9 +274,7 @@ server <- function(input, output) {
             axis.text = element_text(size = 12),
             axis.title = element_text(size = 12))
   })
-  
 }
 
 
 shinyApp(ui, server)
-
